d04b09966af10d734a47e20390933be56f698d40
Rıdvan Örsvuran <flasherdn@gmail.com>
2010-01-23 20:08:04 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(name="pardus-disk-manager-gtk",
+      version="0.1",
+      packages = ["disk_manager_gtk"],
+      scripts = ["disk-manager-gtk.py"],
+      description= "Pardus Disk Manager's gtk port",
+      author="Rıdvan Örsvuran",
+      author_email="flasherdn@gmail.com"
+)
